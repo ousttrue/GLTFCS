f9db3a2df1ec79a5b72ef244b1544c3b03a78c1e
ousttrue <ousttrue@gmail.com>
2019-05-22 18:29:17 +0900

    Add GLTFCS.Viewer

---
+++ GLTFCS.Viewer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GLTFCS.Viewer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
